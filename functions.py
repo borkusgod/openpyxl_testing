78748d0920e3f7449a3144d657d1e47e65283590
borkusgod <lightraven.bourque@gmail.com>
2023-05-29 15:11:44 -0300

    Getting all the files in place and staring to lay out features

---
+++ functions.py
@@ -0,0 +1,12 @@
+# ./term_pys/openpyxl_testing/functions.py
+
+def file_import():
+    pass
+
+
+def file_export():
+    pass
+
+
+def search_for():
+    pass
---
+++ main.py
@@ -0,0 +1,3 @@
+# ./term_pys/openpyxl_testing main.py
+
+load_to_mem = file_import()
